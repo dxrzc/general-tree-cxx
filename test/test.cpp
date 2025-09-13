2c36b2e0096a96eca0b124917f422345659c6c71
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-09-13 13:35:18 -0500

    test: refactor node class tests using subcases

--- test/test.cpp
+++ test/test.cpp
@@ -26,103 +26,118 @@ TEST_SUITE("general_tree::node")
 		REQUIRE_EQ(node, root_node);
 	}
 
-	TEST_CASE("node::data() -- throw invalid argument if node is null")
+	TEST_CASE("node::data()")
 	{
-		general_tree<int> tree;
-		CHECK_THROWS_AS(tree.root().data(), std::invalid_argument);
+		SUBCASE("throw invalid argument if node is null")
+		{
+			general_tree<int> tree;
+			CHECK_THROWS_AS(tree.root().data(), std::invalid_argument);
+		}
 	}
 
-	TEST_CASE("node::child(index) - throw invalid argument if node null")
+	TEST_CASE("node::child(index)")
 	{
-		general_tree<int> tree;
-		CHECK_THROWS_AS(tree.root().child(2), std::invalid_argument);
+		SUBCASE("throw invalid argument if node is null")
+		{
+			general_tree<int> tree;
+			CHECK_THROWS_AS(tree.root().child(2), std::invalid_argument);
+		}
+
+		SUBCASE("return the requested child")
+		{
+			general_tree<int> tree(1);
+			auto left = tree.insert_left_child(tree.root(), 2);
+			auto right = tree.insert_right_sibling(left, 3);
+
+			REQUIRE_EQ(tree.root().child(0), left);
+			REQUIRE_EQ(tree.root().child(1), right);
+		}
+
+		SUBCASE("return null node when node has no children")
+		{
+			general_tree<int> tree(1);
+			auto root = tree.root();
+
+			auto child0 = root.child(0);
+			REQUIRE(child0.is_null());
+		}
 	}
 
-	TEST_CASE("node::child(index) - return the requested child")
+	TEST_CASE("node::children_count()")
 	{
-		general_tree<int> tree(1);
-		auto left = tree.insert_left_child(tree.root(), 2);
-		auto right = tree.insert_right_sibling(left, 3);
+		SUBCASE("throw invalid argument if node is null")
+		{
+			general_tree<int> tree;
+			CHECK_THROWS_AS(tree.root().children_count(), std::invalid_argument);
+		}
 
-		REQUIRE_EQ(tree.root().child(0), left);
-		REQUIRE_EQ(tree.root().child(1), right);
+		SUBCASE("return 0 if node has no children")
+		{
+			general_tree<int> tree(1);
+			REQUIRE_EQ(tree.root().children_count(), 0);
+		}
+
+		SUBCASE("return the number of children")
+		{
+			general_tree<int> tree(1);
+			auto root = tree.root();
+
+			tree.insert_left_child(root, 2);
+			tree.insert_right_sibling(root.left_child(), 3);
+			tree.insert_right_sibling(root.left_child().right_sibling(), 4);
+
+			REQUIRE_EQ(root.children_count(), 3);
+		}
 	}
 
-	TEST_CASE("node::child(index) - return null node when node has no children")
+	TEST_CASE("node::depth()")
 	{
-		general_tree<int> tree(1);
-		auto root = tree.root();
+		SUBCASE("return the depth of the current node")
+		{
+			general_tree<int> tree(1);
+			auto root = tree.root();
+			auto child = tree.insert_left_child(root, 2);
+			auto grandchild = tree.insert_left_child(child, 3);
 
-		auto child0 = root.child(0);
-		REQUIRE(child0.is_null());
+			REQUIRE_EQ(child.depth(), 1);
+			REQUIRE_EQ(grandchild.depth(), 2);
+		}
+
+		SUBCASE("throw invalid argument if node is null")
+		{
+			general_tree<int> tree;
+			CHECK_THROWS_AS(tree.root().depth(), std::invalid_argument);
+		}
+
+		SUBCASE("return 0 for root node")
+		{
+			general_tree<int> tree(42);
+			REQUIRE_EQ(tree.root().depth(), 0);
+		}
 	}
 
-	TEST_CASE("node::children_count() - throw invalid argument if node is null")
+	TEST_CASE("node::descendants_count()")
 	{
-		general_tree<int> tree;
-		CHECK_THROWS_AS(tree.root().children_count(), std::invalid_argument);
-	}
+		SUBCASE("throw invalid argument if node is null")
+		{
+			general_tree<int> tree;
+			CHECK_THROWS_AS(tree.root().descendants_count(), std::invalid_argument);
+		}
 
-	TEST_CASE("node::children_count - return 0 if 0 children")
-	{
-		general_tree<int> tree(1);
-		REQUIRE_EQ(tree.root().children_count(), 0);
-	}
+		SUBCASE("return the number of descendants")
+		{
+			general_tree<int> tree(1);
+			auto root = tree.root();
+			auto child1 = tree.insert_left_child(root, 2);             // 1
+			auto child2 = tree.insert_right_sibling(child1, 3);        // 2
+			auto grandchild1 = tree.insert_left_child(child1, 4);      // 3
+			auto grandchild2 = tree.insert_right_sibling(grandchild1, 5); // 4
+			tree.insert_left_child(child2, 6);                         // 5
 
-	TEST_CASE("node::children_count() - return the number of children")
-	{
-		general_tree<int> tree(1);
-		auto root = tree.root();
-
-		tree.insert_left_child(root, 2);
-		tree.insert_right_sibling(root.left_child(), 3);
-		tree.insert_right_sibling(root.left_child().right_sibling(), 4);
-
-		REQUIRE_EQ(root.children_count(), 3);
-	}
-
-	TEST_CASE("node::depth() - return the depth of the current node")
-	{
-		general_tree<int> tree(1);
-		auto root = tree.root();
-		auto child = tree.insert_left_child(root, 2);
-		auto grandchild = tree.insert_left_child(child, 3);
-
-		REQUIRE_EQ(child.depth(), 1);
-		REQUIRE_EQ(grandchild.depth(), 2);
-	}
-
-	TEST_CASE("node::depth() - throw invalid argument if node is null")
-	{
-		general_tree<int> tree;
-		CHECK_THROWS_AS(tree.root().depth(), std::invalid_argument);
-	}
-
-	TEST_CASE("node::depth() - return 0 for root node")
-	{
-		general_tree<int> tree(42);
-		REQUIRE_EQ(tree.root().depth(), 0);
-	}
-
-	TEST_CASE("node::descendants_count() - throw invalid argument if node is null")
-	{
-		general_tree<int> tree;
-		CHECK_THROWS_AS(tree.root().descendants_count(), std::invalid_argument);
-	}
-
-	TEST_CASE("node::descendants_count() - return the number of descendants")
-	{
-		general_tree<int> tree(1);
-		auto root = tree.root();
-		auto child1 = tree.insert_left_child(root, 2); // 1
-		auto child2 = tree.insert_right_sibling(child1, 3); // 2
-		auto grandchild1 = tree.insert_left_child(child1, 4); // 3
-		auto grandchild2 = tree.insert_right_sibling(grandchild1, 5); // 4
-		tree.insert_left_child(child2, 6); // 5
-
-		REQUIRE_EQ(root.descendants_count(), 5);
-		REQUIRE_EQ(child1.descendants_count(), 2);
-		REQUIRE_EQ(grandchild1.descendants_count(), 0);
+			REQUIRE_EQ(root.descendants_count(), 5);
+			REQUIRE_EQ(child1.descendants_count(), 2);
+			REQUIRE_EQ(grandchild1.descendants_count(), 0);
+		}
 	}
 }
 
