ec6f9e31c527d14729ee88889584044c8d8ed2b0
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-09-13 12:54:30 -0500

    test: add tests for insert tree as right sibling fn

--- test/test.cpp
+++ test/test.cpp
@@ -192,3 +192,84 @@ TEST_SUITE("general_tree::emplace_left_child(...args)")
 	}
 }
 
+TEST_SUITE("general_tree::insert_right_sibling(node, tree)")
+{
+	TEST_CASE("return invalid argument if destiny node is a root")
+	{
+		general_tree<int> tree(0);
+		auto destiny_node = tree.root();
+		general_tree<int> new_tree(100);
+		REQUIRE_THROWS_AS(tree.insert_right_sibling(destiny_node, new_tree), std::invalid_argument);
+	}
+
+	TEST_CASE("return invalid argument if destiny node is nullptr")
+	{
+		general_tree<int> tree(1);
+		auto destiny_node = tree.root().left_child();
+		REQUIRE(destiny_node.is_null());
+
+		general_tree<int> new_tree(100);
+		REQUIRE_THROWS_AS(tree.insert_right_sibling(destiny_node, new_tree), std::invalid_argument);
+	}
+
+	TEST_CASE("return null node if tree is empty")
+	{
+		general_tree<int> tree(1);
+		tree.insert_left_child(tree.root(),  2);
+
+		general_tree<int> empty_tree;
+		auto node = tree.insert_right_sibling(tree.root().left_child(), empty_tree);
+		REQUIRE(node.is_null());
+	}
+
+	TEST_CASE("inserted tree is empty after operation")
+	{
+		general_tree<int> tree(1);
+		tree.insert_left_child(tree.root(), 2);
+
+		general_tree<int> new_tree(2);
+		new_tree.insert_left_child(new_tree.root(), 3);
+		new_tree.insert_left_child(new_tree.root().left_child(), 4);
+		REQUIRE_FALSE(new_tree.empty());
+
+		tree.insert_right_sibling(tree.root().left_child(), new_tree);
+		REQUIRE(new_tree.empty());
+	}
+
+	TEST_CASE("inserted tree root should be the destiny node right sibling")
+	{
+		general_tree<int> tree(1);
+		auto destiny_node = tree.insert_left_child(tree.root(), 2);
+
+		general_tree<int> new_tree(2);		
+		auto inserted_tree_root = new_tree.root();
+
+		tree.insert_right_sibling(destiny_node, new_tree);
+		REQUIRE_EQ(destiny_node.right_sibling(), inserted_tree_root);
+	}	
+
+	TEST_CASE("destiny node parent is now the parent of the inserted tree")
+	{
+		general_tree<int> tree(1);
+		auto destiny_node = tree.insert_left_child(tree.root(), 2);
+		auto destiny_node_parent = tree.root();
+
+		general_tree<int> new_tree(2);
+		auto inserted_tree_root = new_tree.root();
+
+		tree.insert_right_sibling(destiny_node, new_tree);
+		REQUIRE_EQ(destiny_node_parent, inserted_tree_root.parent());
+	}
+
+	TEST_CASE("return root node of the inserted tree")
+	{
+		general_tree<int> tree(1);
+		tree.insert_left_child(tree.root(), 2);
+
+		general_tree<int> new_tree(2);
+		auto inserted_tree_root = new_tree.root();
+
+		auto node = tree.insert_right_sibling(tree.root().left_child(), new_tree);
+		REQUIRE_EQ(node, inserted_tree_root);
+	}
+}
