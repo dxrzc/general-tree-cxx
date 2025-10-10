d13c84d013d2d420f11d83934aa1ce574d456892
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-09 20:48:33 -0500

    test: improve create_root and emplace_root tests coverage

--- test/specs/create-root.test.cpp
+++ test/specs/create-root.test.cpp
@@ -19,7 +19,7 @@ TEST_SUITE("general_tree::create_root(data)")
         REQUIRE_EQ(empty_tree.root().data(), value);
     }
 
-    TEST_CASE_FIXTURE(CounterFixture, "do not make copies when data is an rvalue")
+    TEST_CASE_FIXTURE(CounterFixture, "correct move semantics")
     {
         general_tree<Counter> counters;
         Counter c;
@@ -27,16 +27,39 @@ TEST_SUITE("general_tree::create_root(data)")
         CHECK_EQ(Counter::copy_constructor_calls, 0);
         CHECK_EQ(Counter::move_constructor_calls, 1);
     }
+
+    TEST_CASE_FIXTURE(CounterFixture, "temporary value triggers move")
+    {
+        general_tree<Counter> gt;
+        gt.create_root(Counter{});
+        CHECK_EQ(Counter::copy_constructor_calls, 0);
+        CHECK_EQ(Counter::move_constructor_calls, 1);
+    }
 }
 
 TEST_SUITE("general_tree::emplace_root(...args)")
 {
-    TEST_CASE_FIXTURE(CounterFixture, "data is constructed inside node with no copies")
+    TEST_CASE_FIXTURE(CounterFixture, "does not make copies")
     {
         general_tree<Counter> counters;
         counters.emplace_root("string", 0);
         CHECK_EQ(counters.root().data().get_int(), 0);
         CHECK_EQ(counters.root().data().get_string(), "string");
         REQUIRE_EQ(Counter::copy_constructor_calls, 0);
+    }
+
+    TEST_CASE("throw runtime error if root already exists")
+    {
+        general_tree<int> empty_tree;
+        empty_tree.emplace_root(1);
+        CHECK_THROWS_AS(empty_tree.create_root(2), std::runtime_error);
+    }
+
+    TEST_CASE("data is stored in root node")
+    {
+        general_tree<int> empty_tree;
+        const int value = 123;
+        empty_tree.emplace_root(value);
+        REQUIRE_EQ(empty_tree.root().data(), value);
     }
 }
