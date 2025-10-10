af6f76c04df6c78c45330847fe2a4deaf7a6a676
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-09 20:38:18 -0500

    test: improve constructors tests coverage

--- test/specs/constructors.test.cpp
+++ test/specs/constructors.test.cpp
@@ -1,8 +1,43 @@
 #include "doctest.h"
 #include "general-tree.h"
 #include "utils/fixtures/counter.fixture.h"
+#include <utility>
 
-TEST_SUITE("general_tree::general_tree(general_tree&&)")
+TEST_SUITE("general_tree::general_tree()")
+{
+    TEST_CASE("tree is empty by default")
+    {
+        general_tree<int> gt;
+        REQUIRE(gt.empty());
+    }
+}
+
+TEST_SUITE("general_tree::general_tree(root_value)")
+{
+    TEST_CASE("root_value is now the root")
+    {
+        const int root_value = 100201;
+        general_tree<int> gt(root_value);
+        REQUIRE_EQ(gt.root().data(), root_value);
+    }
+
+    TEST_CASE_FIXTURE(CounterFixture, "correct move semantics")
+    {
+        Counter counter;
+        general_tree<Counter> gt(std::move(counter));
+        REQUIRE_EQ(Counter::move_constructor_calls, 1);
+        REQUIRE_EQ(Counter::copy_constructor_calls, 0);
+    }
+
+    TEST_CASE_FIXTURE(CounterFixture, "construction from temporary value triggers move")
+    {
+        general_tree<Counter> gt(Counter{});
+        REQUIRE_EQ(Counter::move_constructor_calls, 1);
+        REQUIRE_EQ(Counter::copy_constructor_calls, 0);
+    }
+}
+
+TEST_SUITE("general_tree::general_tree(general_tree&& rhs)")
 {
     TEST_CASE_FIXTURE(CounterFixture, "do not make any copies or movements")
     {
@@ -34,5 +69,24 @@ TEST_SUITE("general_tree::general_tree(general_tree&&)")
 
         general_tree<int> tree(std::move(rhs));
         REQUIRE_EQ(tree.root(), root_node);
+    }
+
+    TEST_CASE("no errors if rhs tree is empty")
+    {
+        general_tree<int> empty;
+        general_tree<int> gt(std::move(empty));
+    }
+
+    TEST_CASE("moved-from tree can be reused normally afterwards")
+    {
+        general_tree<int> movedfrom(0);
+        movedfrom.insert_left_child(movedfrom.root(), 1);
+        movedfrom.insert_right_sibling(movedfrom.root().left_child(), 2);
+        general_tree<int> gt(std::move(movedfrom));
+
+        movedfrom.create_root(1);
+        movedfrom.insert_left_child(movedfrom.root(), 2);
+        movedfrom.insert_right_sibling(movedfrom.root().left_child(), 3);
+        movedfrom.insert_left_child(movedfrom.root(), 4);
     }
 }
