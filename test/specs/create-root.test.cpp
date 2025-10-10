e1bae23ecea6f401d6106eefc118a0192deb707e
Diego Rodriguez <diegorodriguezc.dev@gmail.com>
2025-10-10 14:17:39 -0500

    refactor: create-root tests

--- test/specs/create-root.test.cpp
+++ test/specs/create-root.test.cpp
@@ -1,65 +1,65 @@
 #include "doctest.h"
 #include "general-tree.h"
-#include "utils/fixtures/counter.fixture.h"
+#include "utils/fixtures/lifecycle-counter.fixture.h"
 
-TEST_SUITE("general_tree::create_root(data)")
+TEST_CASE_FIXTURE(LifecycleCounterFixture, "create_root")
 {
-    TEST_CASE("throw runtime error if root already exists")
+    SUBCASE("throw runtime error if root already exists")
     {
-        general_tree<int> empty_tree;
-        empty_tree.create_root(1);
+        general_tree<int> empty_tree (1);        
         CHECK_THROWS_AS(empty_tree.create_root(2), std::runtime_error);
     }
 
-    TEST_CASE("data is stored in root node")
+    SUBCASE("create one copy of the provided value and store it in root")
     {
-        general_tree<int> empty_tree;
-        const int value = 123;
-        empty_tree.create_root(value);
-        REQUIRE_EQ(empty_tree.root().data(), value);
-    }
+        general_tree<LifecycleCounter> gt;
+        LifecycleCounter value("string11", 11);
+        gt.create_root(value);
 
-    TEST_CASE_FIXTURE(CounterFixture, "correct move semantics")
-    {
-        general_tree<Counter> counters;
-        Counter c;
-        counters.create_root(std::move(c));
-        CHECK_EQ(Counter::copy_constructor_calls, 0);
-        CHECK_EQ(Counter::move_constructor_calls, 1);
-    }
-
-    TEST_CASE_FIXTURE(CounterFixture, "temporary value triggers move")
-    {
-        general_tree<Counter> gt;
-        gt.create_root(Counter{});
-        CHECK_EQ(Counter::copy_constructor_calls, 0);
-        CHECK_EQ(Counter::move_constructor_calls, 1);
+        CHECK_EQ(gt.root().data().get_int(), value.get_int());
+        CHECK_EQ(gt.root().data().get_string(), value.get_string());
+        CHECK_EQ(LifecycleCounter::move_constructor_calls, 0);
+        CHECK_EQ(LifecycleCounter::copy_constructor_calls, 1);
     }
 }
 
-TEST_SUITE("general_tree::emplace_root(...args)")
+TEST_CASE_FIXTURE(LifecycleCounterFixture, "create_root (move)")
 {
-    TEST_CASE_FIXTURE(CounterFixture, "does not make copies")
+    SUBCASE("provided value is stored in root with no copies")
     {
-        general_tree<Counter> counters;
-        counters.emplace_root("string", 0);
-        CHECK_EQ(counters.root().data().get_int(), 0);
-        CHECK_EQ(counters.root().data().get_string(), "string");
-        REQUIRE_EQ(Counter::copy_constructor_calls, 0);
+        LifecycleCounter value("string001", 1);
+        general_tree<LifecycleCounter> gt;
+        gt.create_root(std::move(value));
+
+        CHECK_EQ(gt.root().data().get_int(), 1);
+        CHECK_EQ(gt.root().data().get_string(), "string001");
+        CHECK_EQ(LifecycleCounter::copy_constructor_calls, 0);
+        CHECK_EQ(LifecycleCounter::move_constructor_calls, 1);
     }
 
-    TEST_CASE("throw runtime error if root already exists")
+    SUBCASE("temporary value triggers move")
+    {
+        general_tree<LifecycleCounter> gt;
+        gt.create_root(LifecycleCounter{});
+        CHECK_EQ(LifecycleCounter::copy_constructor_calls, 0);
+        CHECK_EQ(LifecycleCounter::move_constructor_calls, 1);
+    }
+}
+
+TEST_CASE_FIXTURE(LifecycleCounterFixture, "emplace_root")
+{
+    SUBCASE("throw runtime error if root already exists")
     {
         general_tree<int> empty_tree;
         empty_tree.emplace_root(1);
         CHECK_THROWS_AS(empty_tree.create_root(2), std::runtime_error);
     }
 
-    TEST_CASE("data is stored in root node")
+    SUBCASE("provided arguments construct the value in root with no copies")
     {
-        general_tree<int> empty_tree;
-        const int value = 123;
-        empty_tree.emplace_root(value);
-        REQUIRE_EQ(empty_tree.root().data(), value);
+        general_tree<LifecycleCounter> gt;
+        gt.emplace_root("string999", 999);
+        CHECK_EQ(gt.root().data().get_int(), 999);
+        CHECK_EQ(gt.root().data().get_string(), "string999");
     }
 }
