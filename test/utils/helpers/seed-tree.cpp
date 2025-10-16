b1d86e44c8ed24df76561545f21ee927aa0d254d
dxrzc <diegorodriguezc.dev@gmail.com>
2025-10-16 15:50:10 -0500

    refactor: add reference to avoid warning when going through the vector

--- test/utils/helpers/seed-tree.cpp
+++ test/utils/helpers/seed-tree.cpp
@@ -9,7 +9,7 @@ general_tree<LifecycleCounter> seed_tree(std::size_t max)
     general_tree<LifecycleCounter> gt("string0", 0);
     auto root = gt.root();
 
-    // to build level by level
+    // for building level by level
     std::vector<typename general_tree<LifecycleCounter>::node> parents;
     parents.push_back(root);
     std::size_t counter = 1;
@@ -18,25 +18,25 @@ general_tree<LifecycleCounter> seed_tree(std::size_t max)
     {
         std::vector<typename general_tree<LifecycleCounter>::node> next_parents;
 
-        for (auto parent : parents)
+        for (auto& parent : parents)
         {
             if (counter < max)
             {
                 // add left child
                 const std::string string_value = "string" + std::to_string(counter);
                 const int int_value = static_cast<int>(counter);
-                
-                auto left = gt.emplace_left_child(parent, string_value, int_value);                
+
+                auto left = gt.emplace_left_child(parent, string_value, int_value);
                 ++counter;
 
                 next_parents.push_back(left);
-                
+
                 // add a right sibling
                 if (counter < max)
                 {
                     const std::string string_value = "string" + std::to_string(counter);
                     const int int_value = static_cast<int>(counter);
-                    
+
                     auto sibling = gt.emplace_right_sibling(left, string_value, int_value);
                     ++counter;
 
